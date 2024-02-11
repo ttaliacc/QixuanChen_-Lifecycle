81b3750a68e976044f55a3cab84b52639b57a163
ttaliacc <taliac@bu.edu>
2024-02-11 14:22:19 -0500

    complete questions 2.7

--- app/src/main/java/com/example/qixuanchen_lifecycle/MainActivity.kt
+++ app/src/main/java/com/example/qixuanchen_lifecycle/MainActivity.kt
@@ -4,16 +4,32 @@ import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var view: TextView
+    private lateinit var sayHello: Button
+    private lateinit var edit: EditText
 
     private val activityName = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d(activityName, "onCreate is triggered")
-    }
 
+        view = findViewById(R.id.view)
+        sayHello = findViewById(R.id.sayHello)
+        edit = findViewById(R.id.edit)
+
+        sayHello.setOnClickListener {
+            view.text = "Hello"
+            edit.setText("Hello")
+        }
+
+    }
     override fun onStart() {
         super.onStart()
         Log.d(activityName, "onStart is triggered")
